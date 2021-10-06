2ea034271d57ec9f76ff4cf8d5d31af8b029177c
Lina <36717206+linabeee@users.noreply.github.com>
2021-10-06 16:50:59 +0100

    merge

--- init.el
+++ init.el
@@ -1,30 +1,21 @@
 ;; -*- lexical-binding: t; -*-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(flycheck prettier-js add-node-modules-path rjsx-mode sr-speedbar smex ergoemacs-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(show-paren-mode t)
+(auto-save-visited-mode t)
 
 (setq-default mouse-wheel-scroll-amount '(1 ((shift) . 1))
 	      mouse-wheel-progressive-speed nil
 	      mouse-wheel-follow-mouse 't
 	      scroll-step 1
-
 	      create-lockfiles nil
 	      make-backup-files nil
-
-	      org-adapt-indentation nil
-	      org-descriptive-links nil
-
 	      inhibit-splash-screen t)
+
+(require 'recentf)
+(recentf-mode t)
+(global-set-key (kbd "C-x C-r") 'recentf-open-files)
+(add-hook 'after-init-hook (defun my-after-init-hook ()
+			     (recentf-open-files)))
 
 (add-hook 'prog-mode-hook (defun my-prog-mode-hook ()
 			    (display-line-numbers-mode t)
@@ -32,22 +23,8 @@
 
 (add-hook 'text-mode-hook (defun my-text-mode-hook ()
 			    (visual-line-mode t)
-			    (variable-pitch-mode t)))
-
-(add-hook 'after-init-hook (defun my-after-init-hook ()
-			     (recentf-open-files)))
-
-(ido-mode t)
-(show-paren-mode t)
-(auto-save-visited-mode t)
-
-(recentf-mode t)
-(global-set-key (kbd "C-x C-r") 'recentf-open-files)
-
-(defun indent-buffer ()
-  (interactive)
-  (save-excursion
-    (indent-region (point-min) (point-max))))
+			    (variable-pitch-mode t)
+			    (setq-local cursor-style 'bar)))
 
 (defun split-and-follow-horizontally ()
   (interactive)
@@ -63,15 +40,9 @@
   (other-window 1))
 (global-set-key (kbd "C-x 3") 'split-and-follow-vertically)
 
-(set-face-attribute 'fringe nil :background nil)
-(when (eq system-type 'gnu/linux)
-  (tool-bar-mode 0)
-  (set-face-attribute 'default nil :family "DejaVu Sans Mono"
-		      :height 120)
-  (set-face-attribute 'variable-pitch nil :family "Noto Sans"))
-(when (eq system-type 'windows-nt)
-  (cd (getenv "USERPROFILE")))
+(global-set-key (kbd "C-k") #'kill-whole-line)
 
+(require 'cua-base)
 (cua-mode t)
 (define-key cua-global-keymap (kbd "C-z") #'undo)
 (define-key cua-global-keymap (kbd "C-y") nil)
@@ -79,34 +50,59 @@
 (define-key cua-global-keymap (kbd "C-f") #'isearch-forward)
 (define-key cua-global-keymap (kbd "C-w") #'backward-kill-word)
 
+(defun indent-buffer ()
+  (interactive)
+  (save-excursion
+    (indent-region (point-min) (point-max))))
+
+(set-face-attribute 'fringe nil :background (face-background 'default))
+
+(when (eq system-type 'gnu/linux)
+  (tool-bar-mode 0)
+  (set-face-attribute 'default nil :family "DejaVu Sans Mono"
+		      :height 120)
+  (set-face-attribute 'variable-pitch nil :family "Noto Sans"))
+
+(when (eq system-type 'windows-nt)
+  (cd (getenv "USERPROFILE"))
+  (set-face-attribute 'default nil
+		      :family "Consolas"
+		      :height 110)
+  (set-face-attribute 'variable-pitch nil
+		      :family "Tahoma"
+		      :height 110))
+
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 (eval-when-compile
   (add-to-list 'load-path "~/.emacs.d/use-package")
   (require 'use-package))
-;; (setq use-package-always-ensure t)
 
 (use-package undo-tree
   :ensure t
-  :init
-  (global-undo-tree-mode t)
+  :config (global-undo-tree-mode t)
   :bind (:map cua-global-keymap
 	      ("C-z" . undo-tree-undo)
 	      ("C-y" . undo-tree-redo)))
 
+(use-package org
+  :config (setq-default org-adapt-indentation nil
+			org-descriptive-links nil))
+
 (use-package flycheck
   :defer t
   :hook ((prog-mode . flycheck-mode))
-  :config (add-to-list 'flycheck-disabled-checkers 'emacs-lisp-checkdoc))
+  :config (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
 
 (use-package rjsx-mode
   :mode "\\.[m]?js\\'"
-  :init (setq-default
-	 js-chain-indent t
-	 js-indent-level 2
-	 js2-basic-offset 2))
+  :config (setq-default
+	   js-chain-indent t
+	   js-indent-level 2
+	   js2-basic-offset 2))
 
 (use-package add-node-modules-path
   :defer t
