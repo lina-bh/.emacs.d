f6bd1f927bc0ceed32e27cae78ab5dbd8901ee2d
Lina <me@linabee.uk>
2021-10-12 22:37:11 +0100

    commit for subtree

--- init.el
+++ init.el
@@ -1,5 +1,8 @@
 ;; -*- lexical-binding: t; -*-
 
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file t)
+
 (show-paren-mode t)
 (auto-save-visited-mode t)
 (global-tab-line-mode t)
@@ -9,21 +12,25 @@
 	      mouse-wheel-progressive-speed nil
 	      mouse-wheel-follow-mouse 't
 	      scroll-step 1
+	      scroll-preserve-screen-position t
 	      create-lockfiles nil
 	      make-backup-files nil
 	      inhibit-splash-screen t
-	      vc-follow-symlinks t)
+	      vc-follow-symlinks t
+	      frame-title-format "%b"
+	      w32-enable-synthesized-fonts t)
 
 (desktop-save-mode t)
 (require 'recentf)
 (recentf-mode t)
 (global-set-key (kbd "C-x C-r") 'recentf-open-files)
-(add-hook 'after-init-hook (defun my-after-init-hook ()
-			     (recentf-open-files)))
+(add-hook 'desktop-not-loaded-hook (defun my-after-init-hook ()
+				     (recentf-open-files)))
 
 (add-hook 'prog-mode-hook (defun my-prog-mode-hook ()
 			    (display-line-numbers-mode t)
-			    (electric-pair-local-mode t)))
+			    (electric-pair-local-mode t)
+			    (setq-local indicate-empty-lines t)))
 
 (add-hook 'text-mode-hook (defun my-text-mode-hook ()
 			    (visual-line-mode t)
@@ -45,16 +52,11 @@
 (global-set-key (kbd "C-x 3") 'split-and-follow-vertically)
 
 (global-set-key (kbd "C-k") #'kill-whole-line)
+(global-set-key (kbd "C-w") #'backward-kill-word)
 
 (define-key minibuffer-local-map (kbd "C-u") #'backward-kill-sentence)
 
-(require 'cua-base)
-(cua-mode t)
-(define-key cua-global-keymap (kbd "C-z") #'undo)
-(define-key cua-global-keymap (kbd "C-y") nil)
-(define-key cua-global-keymap (kbd "C-s") #'save-buffer)
-(define-key cua-global-keymap (kbd "C-f") #'isearch-forward)
-(define-key cua-global-keymap (kbd "C-w") #'backward-kill-word)
+
 
 (defun indent-buffer ()
   (interactive)
@@ -77,8 +79,8 @@
 		      :family "Consolas"
 		      :height 110)
   (set-face-attribute 'variable-pitch nil
-		      :family "Tahoma"
-		      :height 110))
+		      :family "Calibri"
+		      :height 115))
 
 (require 'package)
 (add-to-list 'package-archives
@@ -88,6 +90,15 @@
 (eval-when-compile
   (add-to-list 'load-path "~/.emacs.d/use-package")
   (require 'use-package))
+
+(use-package cua-base
+  :init (cua-mode t)
+  :bind (:map cua-global-keymap
+	      ("C-z" . undo)
+	      ("C-y" . nil)
+	      ("C-s" . save-buffer)
+	      ("C-f" . isearch-forward)
+	      ("<C-return" . nil)))
 
 (use-package undo-tree
   :ensure t
@@ -100,10 +111,32 @@
   :config (setq-default org-adapt-indentation nil
 			org-descriptive-links nil))
 
+(use-package org-wc
+  :config (fset #'org-wc-put-overlay #'my--org-wc-put-overlay)
+  :bind (:map org-mode-map
+	      ("C-c w" . org-wc-display)))
+
+(defun my--org-wc-put-overlay (wc)
+  (let* ((c 60)
+         (off 0)
+         ov tx)
+    (org-move-to-column c)
+    (unless (eolp) (skip-chars-backward "^ \t"))
+    (skip-chars-backward " \t")
+    (setq ov (make-overlay (1- (point)) (point-at-eol))
+          tx (concat (buffer-substring (1- (point)) (point))
+                     (make-string (+ off (max 0 (- c (current-column)))) ?.)
+                     (org-add-props (format "%s" (number-to-string wc))
+                         (list 'face 'org-wc-overlay))
+                     ""))
+    (overlay-put ov 'display tx)
+    (push ov org-wc-overlays)))
+
 (use-package flycheck
+  :disabled t
   :defer t
   :hook ((prog-mode . flycheck-mode)
-	 (flycheck-error-list-mode visual-line-mode))
+	 (flycheck-error-list-mode . visual-line-mode))
   :config (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
 
 (use-package rjsx-mode
@@ -128,17 +161,3 @@
 (use-package eglot
   :defer t
   :config (setq-default eglot-stay-out-of '(flymake)))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(eglot vterm undo-tree rjsx-mode prettier-js flycheck add-node-modules-path)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
